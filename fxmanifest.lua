ccfba3c98faa07ff115a965a81dfd3e72c320270
CrypticTM99 <67986767+CrypticTM99@users.noreply.github.com>
2022-09-29 20:13:18 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cryptic#2247'
+description 'Simple QB-Tim_Hortons job for your FiveM Server By Cryptic#2247'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/menus.lua'
+}
+
+server_script 'server/*.lua'
